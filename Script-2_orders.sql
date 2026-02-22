1b2779513a4894e36a4666d75bcd4741a0806df4
VaraKind97 <valadiva@mail.ru>
2026-02-22 23:14:03 +0300

    Add files via upload

---
+++ Script-2_orders.sql
@@ -0,0 +1,11 @@
+create table customers
+(
+    id           int auto_increment
+        primary key,
+    name         varchar(50) not null,
+    surname      varchar(50) not null,
+    age          int         null,
+    phone_number varchar(15) null
+);
+
+
