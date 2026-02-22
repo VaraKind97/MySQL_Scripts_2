f7910b9e185db4cbc0e1d08daced1b120e1f8a9a
VaraKind97 <valadiva@mail.ru>
2026-02-22 23:43:51 +0300

    Update Script-2_orders.sql

--- Script-2_orders.sql
+++ Script-2_orders.sql
@@ -1,11 +1,13 @@
-create table customers
-(
-    id           int auto_increment
-        primary key,
-    name         varchar(50) not null,
-    surname      varchar(50) not null,
-    age          int         null,
-    phone_number varchar(15) null
+create table ORDERS (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date date not null,
+    customer_id int not null,
+    product_name varchar(55) not null,
+    amount int(50) not null,
+    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 );
+
 
 
